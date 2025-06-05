2de229e0d32020a9849fd1785869279119753306
BhaveshBhardwaj <bhardwajbhavesh7@Gmail.com>
2025-06-05 22:22:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Main App styles
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
